2dc96d14e70eb2fb75ee246d3d671fc7ed822a6e
Vaesa Tiara El Fatieah <vaesael54@gmail.com>
2025-05-14 12:12:06 +0700

    mengubah hapusK.php

--- hapusK.php
+++ hapusK.php
@@ -6,7 +6,7 @@ $id_produk=$_GET['id_produk'];
 unset($_SESSION['keranjang'][$id_produk]);
 
 echo 
-    header ("Location: produk.php?hapus=sukses");
+    header ("Locaation: produk.php?hapus=sukses");
     exit;
 echo
     header ("Location: manajemen_produk.php?hapus=gagal");
